fd903b7ccd8b1edd00048b6790c3e7a3df51a9b8
Fabio Guardado <fabio.guardado2012@gmail.com>
2026-07-24 20:54:38 -0600

    refactor del listado y vista para crear autores

--- BibliotecaMVC/Controllers/AutoresController.cs
+++ BibliotecaMVC/Controllers/AutoresController.cs
@@ -5,9 +5,7 @@ namespace BibliotecaMVC.Controllers
 {
     public class AutoresController : Controller
     {
-        public IActionResult Index()
-        {
-            List<Autor> autores = new List<Autor>
+        private static List<Autor> _autores = new List<Autor>
             {
                 new Autor { ID = 1, Nombre = "Gabriel", Apellido = "García Márquez", Nacionalidad = "Colombia", FechaNacimiento = new DateTime(1927, 3, 6), Activo = false },
                 new Autor { ID = 2, Nombre = "Robert", Apellido = "Martin", Nacionalidad = "Estados Unidos", FechaNacimiento = new DateTime(1952, 12, 5), Activo = true },
@@ -15,10 +13,39 @@ namespace BibliotecaMVC.Controllers
                 new Autor { ID = 4, Nombre = "Claudia", Apellido = "Lars", Nacionalidad = "El Salvador", FechaNacimiento = new DateTime(1899, 12, 20), Activo = false },
                 new Autor { ID = 5, Nombre = "Stephen", Apellido = "King", Nacionalidad = "Estados Unidos", FechaNacimiento = new DateTime(1947, 9, 21), Activo = true }
             };
+        public IActionResult Index()
+        {
+            // ViewBag.Autores = _autores;
 
-            ViewBag.Autores = autores;
+            return View(_autores);
+        }
 
+        public IActionResult Details(int id)
+        {
+            var autor = _autores.FirstOrDefault(a => a.ID == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return View(autor);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                autor.ID = _autores.Max(a => a.ID) + 1;
+                _autores.Add(autor);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(autor);
         }
     }
 }
