18e8f776f7b57f734ab3b1a67edf328a9a830e9c
Fabio Guardado <fabio.guardado2012@gmail.com>
2026-08-20 21:48:07 -0600

    Refactorizar componentes reutilizables

---
+++ BibliotecaMVC/Models/CounterItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliotecaMVC.Models
+{
+    public class CounterItemViewModel
+    {
+        public int Id { get; set; }
+        public string Header { get; set; }
+        public string Description { get; set; }
+    }
+}
