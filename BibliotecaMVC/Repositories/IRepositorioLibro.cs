4b4c24ba7050ec0ee529ba4d8bb9d154cbc06646
Fabio Guardado <fabio.guardado2012@gmail.com>
2026-08-29 13:21:32 -0600

    cambios de clase semana 6

---
+++ BibliotecaMVC/Repositories/IRepositorioLibro.cs
@@ -0,0 +1,17 @@
+﻿using BibliotecaMVC.Models;
+
+namespace BibliotecaMVC.Repositories
+{
+    public interface IRepositorioLibro
+    {
+        IEnumerable<Libro> ObtenerTodos();
+
+        Libro? ObtenerLibroPorId(int Id);
+
+        Libro Crear(Libro libro);
+
+        void Eliminar(Libro libro);
+
+        int ObtenerIdParaNuevoLibro();
+    }
+}
